e00a3855b53fbaed6c9bb7119072ba54cd15aacf
Todd-WS <todd@wireservers.com>
2026-03-04 06:34:19 -0600

    feat: update package.json and add tailwind preset configuration

--- demos/expo/tailwind.config.js
+++ demos/expo/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "./utils/**/*.{html,js,jsx,ts,tsx,mdx}",
     "./*.{html,js,jsx,ts,tsx,mdx}",
     "./src/**/*.{html,js,jsx,ts,tsx,mdx}",
-    "../../packages/react-natives/src/**/*.{ts,tsx}",
   ],
   presets: [require("@wireservers-ui/react-natives/tailwind-preset")],
   important: "html",
