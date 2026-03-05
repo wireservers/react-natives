e6e211efd4641f4dc9a79ecb82b9da23bbb4d323
Todd-WS <todd@wireservers.com>
2026-03-05 12:25:03 -0700

    feat: add README for @wireservers-ui/react-natives package with installation, setup, usage, and component details

--- demos/expo/app/+html.tsx
+++ demos/expo/app/+html.tsx
@@ -11,10 +11,7 @@ export default function Root({ children }: PropsWithChildren) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {/*
-          Load icon fonts with the exact font-family names that
-          @expo/vector-icons expects on web.
-        */}
+        {/* Load the exact @expo/vector-icons font files from public/fonts/ */}
         <style
           dangerouslySetInnerHTML={{
             __html: `
@@ -23,14 +20,14 @@ export default function Root({ children }: PropsWithChildren) {
                 font-style: normal;
                 font-weight: 400;
                 font-display: swap;
-                src: url(https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2) format('woff2');
+                src: url(/fonts/MaterialIcons.ttf) format('truetype');
               }
               @font-face {
                 font-family: 'MaterialCommunityIcons';
                 font-style: normal;
                 font-weight: 400;
                 font-display: swap;
-                src: url(https://cdn.jsdelivr.net/npm/@mdi/font@7/fonts/materialdesignicons-webfont.woff2) format('woff2');
+                src: url(/fonts/MaterialCommunityIcons.ttf) format('truetype');
               }
             `,
           }}
