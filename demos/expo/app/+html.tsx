428c0f0124bf5a755cc4dac2c2728b56d36e6f2f
todd-ws <todd@wireservers.com>
2026-03-12 20:22:30 -0500

    Integrate Google Tag Manager for enhanced tracking and analytics (#41)
    * feat: add Clarity tracking code for enhanced analytics
    
    * Integrate Google Tag Manager for enhanced tracking and analytics (#40)
    
    * feat: integrate Google Tag Manager for improved tracking and analytics
    
    * feat: fixed suggestions from PR review.

--- demos/expo/app/+html.tsx
+++ demos/expo/app/+html.tsx
@@ -128,19 +128,6 @@ export default function Root({ children }: PropsWithChildren) {
             `,
           }}
         />
-
-        {/* Clarity tracking code for https://www.reactnatives.dev/ */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "vtvmzxw2aa");
-            `,
-          }}
-        />
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
